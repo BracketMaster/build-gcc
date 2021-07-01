41d4a1d7a3466c82ee2f02f6d6280feb733daed4
Yehowshua Immanuel <yehowshuai@chipeleven.com>
2021-07-01 17:46:21 -0400

    update download link

--- 1-download.sh
+++ 1-download.sh
@@ -20,4 +20,6 @@ curl -L ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.0.3.tar.gz | tar xzf -
 curl -L ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2 | tar xjf -
 curl -L https://ftp.gnu.org/gnu/sed/sed-4.7.tar.xz | tar xJf -
 curl -L http://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.gz | tar xzf -
-curl -L https://ftp.gnu.org/gnu/gcc/gcc-11.1.0/gcc-11.1.0.tar.xz | tar xzf -
+curl -L https://github.com/iains/gcc-darwin-arm64/tarball/master --output gcc-11.1.0.tar
+mkdir -p gcc-11.1.0
+tar xzf gcc-11.1.0.tar -C gcc-11.1.0 --strip-components 1
